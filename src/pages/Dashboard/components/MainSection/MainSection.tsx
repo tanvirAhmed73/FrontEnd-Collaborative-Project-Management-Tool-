114b07884595ffe0c09f70399446b9d7fc0463ca
tanvirAhmed73 <tanvir.073.ahmed@gmail.com>
2025-01-10 19:32:02 +0600

    unneccesary import remove2

--- src/pages/Dashboard/components/MainSection/MainSection.tsx
+++ src/pages/Dashboard/components/MainSection/MainSection.tsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ProjectCard from "../ProjectCard/ProjectCard";
 import ProjectCreationForm from '../ProjectCreationForm/ProjectCreationForm';
 import { Project } from '../../../../interfaces/project';
 import { X } from 'lucide-react';
-import AuthContext from '../../../../context/AuthProvider';
 
 
 
